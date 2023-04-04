85a5d8637dac2c35536e6259ad1636bc5fdcd7f3
sydney-clark <sydney.natasha.clark@gmail.com>
2023-04-04 10:14:01 -0700

    created superuser

---
+++ travelTracker/travelApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Travel(models.Model):
+    location = models.CharField(max_length=50)
+    type = models.CharField(max_length=30)
+    date = models.CharField(max_length=15)
+    address = models.CharField(max_length=50)
+    contact = models.CharField(max_length=100)
+    def __str__(self): return self.location
+    def __str__(self): return self.type
+    def __str__(self): return self.date
+    def __str__(self): return self.address
+    def __str__(self): return self.contact
+
